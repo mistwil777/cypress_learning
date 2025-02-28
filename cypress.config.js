583b7ca191eae94dacd4d53c9a19e4ae85c73134
mistwil777 <leroulierwilfried@gmail.com>
2025-02-28 10:58:14 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    excludeSpecPattern: '**/examples/*.cy.js',
+    excludeSpecPattern: '**/examples/**.cy.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
